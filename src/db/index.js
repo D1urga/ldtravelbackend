3253e095ffc920d2523c3cf0fa4ef02b53bc0296
unknown <64825962+D1urga@users.noreply.github.com>
2024-05-02 18:53:00 +0530

    file added

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      "mongodb+srv://anoop:anoop1234@database.sjmay8w.mongodb.net/databases?retryWrites=true&w=majority&appName=database"
+      "mongodb+srv://anoop:anoop1234@cluster0.0kjgjta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log(`mongodb connected ${connectionInstance.connection.host}`);
   } catch (error) {
